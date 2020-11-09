bee84c63e06a5cb574190c6fee137c280fcd88bb
konnovK <k.kirialosk@gmail.com>
2020-11-09 05:42:39 +0300

    triangles in figures

--- src/Task4/figure/RegularTriangle.java
+++ src/Task4/figure/RegularTriangle.java
@@ -1,25 +1,8 @@
 package Task4.figure;
 
-public class RegularTriangle implements Figure {
-
-    private final double a;
+public class RegularTriangle extends Triangle {
 
     public RegularTriangle(double a) {
-        this.a = a;
-    }
-
-    @Override
-    public double area() {
-        return Math.sqrt(3) * a*a / 4;
-    }
-
-    @Override
-    public double perimeter() {
-        return 3 * a;
-    }
-
-    @Override
-    public String info() {
-        return "я правильный треугольник со стороной " + a;
+        super(a, a, a);
     }
 }
--- src/Task4/figure/RightTriangle.java
+++ src/Task4/figure/RightTriangle.java
@@ -1,27 +1,8 @@
 package Task4.figure;
 
-public class RightTriangle implements Figure {
-
-    private final double a;
-    private final double b;
+public class RightTriangle extends Triangle {
 
     public RightTriangle(double a, double b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    @Override
-    public double area() {
-        return (a * b) / 2;
-    }
-
-    @Override
-    public double perimeter() {
-        return a + b + Math.sqrt(a*a + b*b);
-    }
-
-    @Override
-    public String info() {
-        return "я прямоугольный треугольник со сторонами " + a + " и " + b;
+        super(a, b, Math.sqrt(a*a + b*b));
     }
 }
