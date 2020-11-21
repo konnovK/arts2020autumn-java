b93b77556d0b61897dd8132f09678214dc324a49
konnovK <k.kirialosk@gmail.com>
2020-11-21 15:14:27 +0300

    printable refactoring

--- src/inheritance/printable/PrintableLetter.java
+++ src/inheritance/printable/PrintableLetter.java
@@ -3,6 +3,6 @@ package inheritance.printable;
 public class PrintableLetter extends PrintableString {
 
     public PrintableLetter(char c, int count) {
-        super(String.valueOf(c).repeat(Math.max(0, count)));
+        super(String.valueOf(c).repeat(count));
     }
 }
